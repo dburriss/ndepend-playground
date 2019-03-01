50f47683369c9145b81ba1fd31fa4ac413d58c75
Devon Burriss <devon@chimplab.co>
2019-03-01 06:25:40 +0100

    Initial commit

---
+++ csharp/simple/src/ClassWithIO.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Data;
+using System.IO;
+using Microsoft.Data.Sqlite;
+
+namespace simple
+{
+    public class ClassWithIO : IDisposable
+    {
+        readonly IDbConnection db;
+        public ClassWithIO()
+        {
+            db = new SqliteConnection("Data Source=testdb.sqlite;");
+        }
+
+        public void DatabaseAccess()
+        {
+            db.Open();
+            db.Close();
+        }
+
+        public void FileAccess()
+        {
+            if (!Directory.Exists(@"C:\Temp"))
+                throw new InvalidOperationException("No Temp");
+        }
+
+        public void Dispose()
+        {
+            db.Dispose();
+        }
+
+    }
+}
---
+++ csharp/simple/src/Result.cs
@@ -0,0 +1,31 @@
+﻿namespace simple
+{
+    public class Result
+    {
+        public bool IsSuccess { get; private set; }
+
+        private Result(bool successful)
+        {
+            IsSuccess = successful;
+        }
+
+        public static Result Success => new Result(true);
+        public static Result Failure => new Result(false);
+    }
+
+    public class Result<T>
+    {
+        public bool IsSuccess { get; private set; }
+        public T Value { get; private set; }
+
+        private Result(T value, bool successful)
+        {
+            Value = value;
+            IsSuccess = successful;
+        }
+
+        public static Result<T> Success(T value) => new Result<T>(value, true);
+        public static Result<T> Failure() => new Result<T>(default(T), false);
+        public static Result<T> Failure(T defaultValue) => new Result<T>(defaultValue, false);
+    }
+}
